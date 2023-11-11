779d0bbed7cd0ba83a90046a154cdc1b86625519
carlo <carloscanoespinosa01@gmail.com>
2023-11-11 16:57:57 +0100

    Limpieza de la clase Punto y nueva funcionalidad en la clasLecturaEscritura que nos da los archivos que tenemos

--- Practica1_amc/src/practica1_amc/Punto.java
+++ Practica1_amc/src/practica1_amc/Punto.java
@@ -41,30 +41,4 @@ public class Punto {
         return (double) (sqrt((Math.pow(x1 - this.x, 2)) + Math.pow(y1 - this.y, 2)));
     }
 
-    public ArrayList<Punto> rellenarPuntos(ArrayList<Punto> p, int n, boolean mismax) {
-        int num, den;
-        double x, y, aux1;
-
-        Random random = new Random();
-        if (mismax) { //PEOR CASO
-            for (int i = 0; i < n; i++) {
-                aux1 = random.nextDouble() % 1000 + 7; //7 y 1007
-                y = (double) (aux1 / ((double) i + 1 + i * 0.100)); //aux2; //+(i/3.0);
-                num = (int)random.nextDouble() % 3;
-                y += ((i % 500) - num * (random.nextDouble() % 100));
-                x = 1;
-                p.add(new Punto(x, y));
-            }
-        } else { //CASO MEDIO
-            for (int i = 0; i < n; i++) {
-                /*num = rand() % 4000 + 1; //genera un número aleatorio entre 1 y 4000
-                den = rand() % 11 + 7; //genera un aleatorio entre 7 y 17
-                x = num / ((double) den + 0.37); //division con decimales
-                y = (rand() % 4000 + 1) / ((double) (rand() % 11 + 7) + 0.37);
-                p[i] = Punto(x, y, i + 1);*/
-            }
-        }
-        return p;
-    }
-
 }
